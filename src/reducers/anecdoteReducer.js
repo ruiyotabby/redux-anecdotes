944772b242687afbc11e42b11f6a0cbd28173bfd
Ruiyot Abby <ruiyotabby@gmail.com>
2023-09-18 16:06:54 +0300

    Add vote functionality

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -20,10 +20,20 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  return state
+  console.log(state);
+  console.log(action);
+  switch (action.type) {
+    case 'VOTE':
+      const id  = action.payload.id;
+      const anecdoteToChange = state.find(a  => a.id === id);
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      };
+      return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote);
+    default:
+      return state;
+  }
 }
 
 export default reducer
